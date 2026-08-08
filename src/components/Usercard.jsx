1b13629f0ee863719946a85eda6e77f5884211b8
Bela-Mina <belaandrosbela@example.com>
2026-08-09 00:01:17 +0300

    complete Profile App

---
+++ src/components/Usercard.jsx
@@ -0,0 +1,11 @@
+
+function Usercard({name, age, skill}) {
+    return (
+        <div>
+            <h1>My Name is {name} I am {age} years old and My Skill is {skill} </h1>
+        
+            
+        </div>
+    )
+}
+export default Usercard;
