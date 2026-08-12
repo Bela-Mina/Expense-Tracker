c60f84070be00b68716490c1db68c450d9035e97
Bela-Mina <belaandrosbela@example.com>
2026-08-12 10:12:21 +0300

    we start work experss tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,13 @@
-import React from "react";
-import Product from "./Product";
-import Usercard from"./components/Usercard";
+// import Alxstudy from "./components/Alxstudy.jsx";
+import ExpenseTracker from "./components/ExpenseTracker.jsx";
+//import Learn from "./components/Learn.jsx";
 function App() {
-
-  const user = [
-    {
-      name: "Abel",
-      age: 22,
-      skill: "Front-End Developer",
-    },
-    {
-      name: "John",
-      age: 30,
-      skill: "Designer"
-    },
-    {
-      name: "sara",
-      age: 22,
-      skill: "Developer"
-    }
-  ]
-  return (
-    <div>
-    
-      {user.map((user) => {
-        return(
-          <Usercard
-            name={user.name}
-            age={user.age}
-            skill={user.skill}
-          />
-        )
-      })}
-    </div>
-  )
+    return (
+        <div>
+            <Learn />
+            <ExpenseTracker />
+        </div>
+    );
 }
+
 export default App;
-
-
-{/* <Usercard name="Abel"
-          age={22}
-          skill="Front-End Developer"
-      />
-
-      <Usercard name="John" age={30} skill="Designer" />
-
-      <Usercard name="sara"
-          age={22}
-          skill="Developer"
-      /> */}
-
-      {/* <Product
-        name="Future Project to be" 
-        decription="Proffestional Developer"
-        Risk="You must Sacrifice up to date"
-      />
-      <Product
-        name="Front-End Developer Goal One" 
-        decription="You Must Study Html, Css, JavaScript"
-        Risk="You Work real world App"
-      />
-      <Product
-        name="Back-End Developer Goal Two" 
-        decription="You Must Study Node js Express, mangodb"
-        Risk="You Must Connect Your Front-End App To Back-End"
-      /> */}
---
+++ src/components/ExpenseTracker.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function ExpenseTracker() {
+    const [expense, setExpense] = useState("");
+
+    
+    return (
+        <div>
+            <h1>Expense Tracker</h1>
+
+            <input
+                type="text"
+                placeholder="Enter Your Expense"
+                value={expense}
+                onChange={(event) => setExpense(event.target.value)}
+            />
+                <button onClick={theExpense}>
+                        Add Expense
+                </button>
+                
+             {expense.map((expense, index)=> (
+                <p key={index}>{expense}</p>
+             ))
+              
+            }
+
+            
+        </div>
+    );
+}
+
+export default ExpenseTracker;
